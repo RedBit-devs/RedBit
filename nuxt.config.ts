88060e71ce35f7348fc903383a57a6b61de2d728
PetiDev <peteristvanh@gmail.com>
2024-12-02 20:00:22 +0100

    feat: :lipstick: Created assets folder, base global css written, fonts loaded

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  css: [
+    '~/assets/css/main.css',
+    '~/assets/css/fonts.css'
+  ],
   devtools: { enabled: true },
   modules: ["@prisma/nuxt"],
   prisma: {
