ef856b395adca5ff666f84dd6f7acb0a4bbc1aef
PetiDev <peteristvanh@gmail.com>
2024-12-11 18:06:48 +0100

    feat: :sparkles: navbar
    Some part of this commit was added to a commit before by mistake (commit id 24241bf10b1773e3041cc94286c13a9e47d5ae50)

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,17 @@
     <nav>
         <h1>RedBit</h1>
         <span class="spacer"></span>
+
+        <label id="isMenuShownLabel"for="isMenuShown"><Icon name="mdi:menu" size="50"/></label>
+        <input type="checkbox" id="isMenuShown">
         <menu>
-            <NuxtLink class="btn primary" to="/loginPage">chat</NuxtLink>
+            <NuxtLink class="btn primary" to="/loginPage">Chat</NuxtLink>
             <NuxtLink class="btn " to="#title">Home</NuxtLink>
             <NuxtLink class="btn " to="#about">About</NuxtLink>
             <NuxtLink class="btn " to="#developers">Contacts</NuxtLink>
         </menu>
     </nav>
-    <slot />
+    <slot style="margin-top: 3.5rem;" />
 </template>
 
 <style scoped>
@@ -17,13 +20,21 @@ nav{
     position: sticky;
     top: 0;
     background-color: var(--clr-ui-secondary);
+    z-index: 42;
 
-    height: 55px;
+    height: 3.5rem;
 
     display: flex;
     align-items: center;
     padding-inline: 1rem;
 }
+#isMenuShown{
+    display: none;
+}
+#isMenuShownLabel{
+    display: none;
+}
+
 .spacer {
     display: flex;
     flex-grow: 1;
@@ -31,5 +42,28 @@ nav{
 menu{
     display: flex;
     gap: 1rem;
+    align-items: center;
+}
+@media (max-width:590px) {
+    #isMenuShownLabel{
+    display: block;
+}
+    menu{
+        background-color: var(--clr-ui-secondary);
+        flex-direction: column;
+        position: absolute;
+        top: 3.5rem;
+        right: 0rem;
+        transform: scaleY(0);
+        transform-origin: top;
+        transition: transform ease .3s;
+        padding-inline: 1rem;
+        padding-bottom: 1rem;
+        border-bottom-left-radius: var(--border-rounded);
+    }
+    input:checked ~ menu, menu:focus-within{
+        transform: scaleY(1);
+    }
+    
 }
 </style>
