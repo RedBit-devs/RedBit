24241bf10b1773e3041cc94286c13a9e47d5ae50
PetiDev <peteristvanh@gmail.com>
2024-12-11 17:22:51 +0100

    feat: :construction: button classes in the main css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,13 @@
 <template>
     <nav>
-        <h1>Fr a navbar</h1>
+        <h1>RedBit</h1>
+        <span class="spacer"></span>
+        <menu>
+            <NuxtLink class="btn primary" to="/loginPage">chat</NuxtLink>
+            <NuxtLink class="btn " to="#title">Home</NuxtLink>
+            <NuxtLink class="btn " to="#about">About</NuxtLink>
+            <NuxtLink class="btn " to="#developers">Contacts</NuxtLink>
+        </menu>
     </nav>
     <slot />
 </template>
@@ -10,5 +17,19 @@ nav{
     position: sticky;
     top: 0;
     background-color: var(--clr-ui-secondary);
+
+    height: 55px;
+
+    display: flex;
+    align-items: center;
+    padding-inline: 1rem;
+}
+.spacer {
+    display: flex;
+    flex-grow: 1;
+}
+menu{
+    display: flex;
+    gap: 1rem;
 }
 </style>
