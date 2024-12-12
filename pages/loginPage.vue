cea7d2a0ed7a2bf066eac8d739034f7d7b1a6805
Kevin <kevinszigetvari40@gmail.com>
2024-12-12 18:26:47 +0100

    Modified te register page and did some style changes on the login pageaa

--- pages/loginPage.vue
+++ pages/loginPage.vue
@@ -16,15 +16,13 @@
                         </div>
                     </div>
                     <div class="submit">
-                        <button class="buttonSubmit"> Submit</button>
+                        <button class="btn ui-secondary"> Submit</button>
                     </div>
                 </div>
                 <div class="register">
                     <label>Not registered yet?</label>
-                    <NuxtLink to="/registerPage">
-                        <button class="buttonRegister">
-                            Resgister
-                        </button>
+                    <NuxtLink to="/registerPage" class="btn secondary">
+                        Register
                     </NuxtLink>
                 </div>
             </div>
@@ -38,13 +36,12 @@ definePageMeta({
 </script>
 
 <style scoped>
-a::after {
-    height: 0;
-    width: 0;
-}
-
 .wrapper {
     background-image: linear-gradient(to bottom right, var(--clr-tertiary), var(--clr-ui-secondary));
+}
+
+.arrow {
+    position: absolute;
 }
 
 .content {
@@ -52,14 +49,13 @@ a::after {
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    min-height: 90vh;
-    font-family: Inter;
+    min-height: 100vh;
+    gap: 2rem;
 }
-
 
 h1 {
     text-align: center;
-    margin: 7%;
+    padding-block: 3rem;
     text-transform: uppercase;
     font-weight: 400;
     letter-spacing: 4px;
@@ -67,18 +63,18 @@ h1 {
 }
 
 .login-box {
-    color: white;
     background-color: var(--clr-primary);
     width: 40%;
-    margin-bottom: 1.5rem;
-    border-radius: 8px;
+    border-radius: var(--border-rounded);
     box-shadow: 10px 10px 21px 1px rgb(from var(--clr-text-inverse) r g b / .6);
+
 }
 
 .input-field {
     display: flex;
     flex-direction: column;
     align-items: center;
+    gap: 2rem;
 
 }
 
@@ -86,83 +82,45 @@ h1 {
     display: flex;
     flex-direction: column;
     width: 90%;
-    margin: 2%;
 }
 
-label {
-    font-family: Inter;
-    font-size: 16px;
-}
 
 input {
     border-radius: var(--border-rounded);
     border: none;
-    height: 42px;
+    height: 3rem;
     margin-top: 1%;
-    font-size: 16px;
-    padding: 5px;
+    padding: .5rem;
 }
 
 .submit {
     display: flex;
     justify-content: center;
     align-items: center;
+    margin: 1.5rem;
 }
-
-.buttonSubmit {
-    background-color: var(--clr-ui-secondary);
-    color: white;
-    border-radius: var(--border-rounded);
-    border: none;
-    padding: 10px 25px 10px 25px;
-    width: fit-content;
-    margin: 8%;
-    font-size: 16px;
-    font-family: Inter;
-    transition: .5s;
-}
-
-.buttonSubmit:hover {
-    background-color: var(--clr-ui-primary);
-}
-
 
 .register {
     display: flex;
     background-color: var(--clr-ui-primary);
-    color: white;
     border-radius: var(--border-rounded);
     width: 30%;
-    padding: 0.5rem 1rem 0.5rem 1rem;
+    gap: 1rem;
+    padding: 0.5rem 1rem;
     justify-content: space-between;
     align-items: center;
     box-shadow: 10px 10px 21px 1px rgb(from var(--clr-text-inverse) r g b / .6);
 }
-
-.buttonRegister {
-    background-color: var(--clr-secondary);
-    border: none;
-    border-radius: var(--border-rounded);
-    padding: 10px 20px 10px 20px;
-    width: fit-content;
-    font-family: Inter;
-    transition: .5s;
-    color: white;
-}
-
-.buttonRegister:hover {
-    background-color: var(--clr-tertiary);
-}
-
 
 @media only screen and (max-width:1100px) {
     .login-box {
         width: 50%;
     }
 
-    input {
-        height: 36px;
+    .register {
+        width: fit-content;
     }
+
 }
 
 @media only screen and (max-width:850px) {
@@ -175,48 +133,14 @@ input {
     }
 
     input {
-        height: 32px;
-    }
-
-    .register {
-        width: fit-content;
-    }
-
-    .input-box {
-        width: fit-content;
-        padding: 10%;
-    }
-
-    h1 {
-        font-size: 30px;
-    }
-
-    .buttonRegister {
-        margin-left: 1rem
+        height: 2.2rem;
     }
 }
 
 @media only screen and (max-width:640px) {
-
-    .content {
-        padding: 1.5rem;
-    }
-
     .login-box {
         width: 100%;
     }
 
-    .register {
-        width: fit-content;
-    }
-
-    .buttonSubmit {
-        padding: 10px 15px 10px 15px;
-    }
-
-    .buttonRegister {
-        padding: 10px 15px 10px 15px;
-        margin-left: 1rem
-    }
 }
 </style>
