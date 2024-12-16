8f3d9f89815280bb800c0546edb65cca6653bf47
413ksz <developer413ksz@gmail.com>
2024-12-16 17:13:13 +0100

    feat: :sparkles: Added user type
    Added user type for future use

---
+++ types/user.d.ts
@@ -0,0 +1,12 @@
+type User = {
+  id: String;
+  username: String;
+  email: String;
+  phone_number: String;
+  profile_picture: String;
+  birthdate: DateTime;
+  first_name: String;
+  last_name: String;
+  description: String;
+  created_at: DateTime;
+};
