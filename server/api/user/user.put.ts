8a123b0137029328460f4ca5fcc12ec5aecc222a
413ksz <developer413ksz@gmail.com>
2024-12-17 11:07:09 +0100

    refactor: :art: Fixed user.put.ts format
    format document

--- server/api/user/user.put.ts
+++ server/api/user/user.put.ts
@@ -1,27 +1,28 @@
-import prisma from "~/lib/prisma"
+import prisma from "~/lib/prisma";
 
 export default defineEventHandler(async (event) => {
-    const newUser : User = await readBody(event) as User
+  const newUser: User = (await readBody(event)) as User;
 
-    if (await IsUserExist(newUser.email)) {
-        return {
-            "message": "User already exist"
-        }}
+  if (await IsUserExist(newUser.email)) {
     return {
-        message: "User created"
-     }
-  })
-
-  async function  IsUserExist(email  : string): Promise<boolean> {
-    const user = await prisma.user.findFirst({  
-        where: {
-            email: email
-        }
-    })
-
-    if (!user) {
-        return false
-    }
-
-    return true
+      message: "User already exist",
+    };
   }
+  return {
+    message: "User created",
+  };
+});
+
+async function IsUserExist(email: string): Promise<boolean> {
+  const user = await prisma.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
+
+  if (!user) {
+    return false;
+  }
+
+  return true;
+}
