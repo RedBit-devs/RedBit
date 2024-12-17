13e80253e944df8ec5aa62de05df4cdf00c71737
413ksz <developer413ksz@gmail.com>
2024-12-17 10:27:02 +0100

    feat: :sparkles: created user exist function
    Created user exist function for checking if the user is already exist in the database

--- server/api/user/user.put.ts
+++ server/api/user/user.put.ts
@@ -1,7 +1,22 @@
+import { usePrismaClient } from '@prisma/nuxt';
 export default defineEventHandler(async (event) => {
+    const { $prisma } = usePrismaClient();
+    const prisma = $prisma;
     const newUser : User = await readBody(event)
 
+     async function  IsUserExist(email  : string): Promise<boolean> {
+        const user = await prisma.user.findFirst({  
+            where: {
+                email: email
+            }
+        })
 
+        if (!user) {
+            return false
+        }
+
+        return true
+      }
     return {
         message: "User created"
      }
