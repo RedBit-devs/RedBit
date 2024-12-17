a1934e12b25da327c21210da86c2472592aa5423
413ksz <developer413ksz@gmail.com>
2024-12-17 15:03:26 +0100

    chore: :construction: Dummy Data for testing user api later on
    Dummy data nothing more nothing less

--- server/api/user/user.put.ts
+++ server/api/user/user.put.ts
@@ -2,7 +2,18 @@ import prisma from "~/lib/prisma";
 import { Prisma } from '@prisma/client'
 
 export default defineEventHandler(async (event) => {
-  const newUser: NewUser = await readBody(event);
+  //const newUser: NewUser = await readBody(event);
+  const newUserData: NewUser = {
+    username: "johnDoe123",
+    email: "johndoe@example.com",
+    phone_number: 1234567890,
+    profile_picture: "https://picsum.photos/200/300",
+    birthdate: new Date("1990-02-12"),
+    first_name: "John",
+    last_name: "Doe",
+    description: "This is a sample user description.",
+    password: "password123"
+  };
 
   try {
     await prisma.user.create({ data: newUser })
@@ -12,6 +23,8 @@ export default defineEventHandler(async (event) => {
         console.log(
           'There is a unique constraint violation, a new user cannot be created with this email because it already exists'
         )
+      }else{
+        `${e.message}`
       }
     }
     throw e
