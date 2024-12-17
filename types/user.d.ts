34e0d3c1dcec9a1e994d26eec5e5cc56db29d390
413ksz <developer413ksz@gmail.com>
2024-12-17 17:16:42 +0100

    chore: :art: Changes to user type
    Added atttributes: id , created_at

--- types/user.d.ts
+++ types/user.d.ts
@@ -1,9 +1,11 @@
-type NewUser = {
+type User = {
+  id: string;
   username: string;
   email: string;
   phone_number: string;
   profile_picture: string;
   birthdate: Date;
+  created_at: Date;
   first_name: string;
   last_name: string;
   description: string;
