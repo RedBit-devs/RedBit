6709187d3a8811e4c220c79d146e4cf9babd7c69
413ksz <developer413ksz@gmail.com>
2024-12-18 20:48:41 +0100

    docs: :memo: Added documentation to user.put functions
    Added documentation to functions explaining the operation of the funtion and the param, return values

--- server/api/user/.put.ts
+++ server/api/user/.put.ts
@@ -51,6 +51,15 @@ export default defineEventHandler(async (event) => {
   };
 });
 
+/**
+ * Hashes the given password using bcrypt algorithm.
+ *
+ * This function generates a salt with a predefined number of rounds
+ * and uses it to hash the provided password. The resulting hash is returned.
+ *
+ * @param {string} password - The password to be hashed.
+ * @returns {Promise<string>} - A promise that resolves to the hashed password.
+ */
 const hashPassword = async (password: string): Promise<string> => {
   const saltRounds = 12;
   const salt = await bcrypt.genSalt(saltRounds);
@@ -62,6 +71,19 @@ const hashPassword = async (password: string): Promise<string> => {
   }
 };
 
+/**
+ * Validates the given password.
+ *
+ * The password must meet the following criteria:
+ * - Contains at least 8 characters
+ * - Includes at least one uppercase letter
+ * - Includes at least one lowercase letter
+ * - Includes at least one digit
+ * - Includes at least one special character (e.g., @$!%*#?&)
+ *
+ * @param {string} password - The password to be validated.
+ * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating if the password is valid.
+ */
 const isPasswordValid = async (password: string): Promise<boolean> => {
   const validPasswordPattern: RegExp =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
@@ -69,6 +91,17 @@ const isPasswordValid = async (password: string): Promise<boolean> => {
   return validPasswordCheck;
 };
 
+/**
+ * Validates the given email address.
+ *
+ * The email must match the following pattern:
+ * - Must contain alphanumeric characters, dots, underscores, pluses, or hyphens before the '@' symbol.
+ * - Must contain a domain name with alphanumeric characters and dots after the '@' symbol.
+ * - Must have a top-level domain of at least two alphabetical characters.
+ *
+ * @param {string} email - The email address to be validated.
+ * @returns {Promise<boolean>} - A promise resolving to a boolean indicating if the email is valid.
+ */
 const isEmailValid = async (email: string): Promise<boolean> => {
   const validEmailPattern: RegExp =
     /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -76,6 +109,17 @@ const isEmailValid = async (email: string): Promise<boolean> => {
   return validEmailCheck;
 };
 
+/**
+ * Validates the given username.
+ *
+ * The username must match the following requirements:
+ * - At least 3 characters long
+ * - At most 32 characters long
+ * - Contains only letters (both uppercase and lowercase), numbers, and underscore
+ *
+ * @param {string} username - The username to be validated.
+ * @returns {Promise<boolean>} - A promise resolving to a boolean indicating if the username is valid.
+ */
 const isUsernameValid = async (username: string): Promise<boolean> => {
   const validUsernamePattern: RegExp = /^[a-zA-Z0-9_]{3,32}/;
   const validUsernameCheck = validUsernamePattern.test(username);
