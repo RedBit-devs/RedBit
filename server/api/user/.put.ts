5fc68945df4e6e456e61567949d0860182928082
413ksz <developer413ksz@gmail.com>
2024-12-18 20:35:44 +0100

    chore: :recycle: Refactor functions
    Refactored functions to callback type functions

--- server/api/user/.put.ts
+++ server/api/user/.put.ts
@@ -4,20 +4,18 @@ import bcrypt from "bcrypt";
 
 export default defineEventHandler(async (event) => {
   const newUser: User = await readBody(event);
-
-  if(!(await isPasswordValid(newUser.password))) {
+  if (!(await isPasswordValid(newUser.password))) {
     return {
       message:
         "Password is not valid - it must contain at least 8 characters, one uppercase letter, one lowercase letter, one number and one special character",
     };
   }
-  if(!(await isEmailValid(newUser.email))) {
+  if (!(await isEmailValid(newUser.email))) {
     return {
-      message:
-        "Email is not in the correct format",
+      message: "Email is not in the correct format",
     };
   }
-  if(!(await isUsernameValid(newUser.username))) {
+  if (!(await isUsernameValid(newUser.username))) {
     return {
       message:
         "Username is not in the correct format it must be between 3 and 32 characters long and can only contain letters, numbers and underscores",
@@ -53,7 +51,7 @@ export default defineEventHandler(async (event) => {
   };
 });
 
-async function hashPassword (password: string): Promise<string> {
+const hashPassword = async (password: string): Promise<string> => {
   const saltRounds = 12;
   const salt = await bcrypt.genSalt(saltRounds);
   try {
@@ -62,25 +60,24 @@ async function hashPassword (password: string): Promise<string> {
   } catch (e) {
     throw e;
   }
-}
+};
 
-async function isPasswordValid(password: string): Promise<boolean> {
-  const validPasswordPattern : RegExp =
+const isPasswordValid = async (password: string): Promise<boolean> => {
+  const validPasswordPattern: RegExp =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
   const validPasswordCheck = validPasswordPattern.test(password);
   return validPasswordCheck;
-}
+};
 
-async function isEmailValid(email: string): Promise<boolean> {
-  const validEmailPattern : RegExp =
-  /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const isEmailValid = async (email: string): Promise<boolean> => {
+  const validEmailPattern: RegExp =
+    /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const validEmailCheck = validEmailPattern.test(email);
   return validEmailCheck;
-}
+};
 
-async function isUsernameValid(username: string): Promise<boolean> {
-  const validUsernamePattern : RegExp =
-  /^[a-zA-Z0-9_]{3,32}/;
+const isUsernameValid = async (username: string): Promise<boolean> => {
+  const validUsernamePattern: RegExp = /^[a-zA-Z0-9_]{3,32}/;
   const validUsernameCheck = validUsernamePattern.test(username);
   return validUsernameCheck;
-}
+};
