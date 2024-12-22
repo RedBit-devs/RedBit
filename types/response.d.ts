ac3c3ebc38020d610206a710ea3bf6168c1df186
413ksz <developer413ksz@gmail.com>
2024-12-22 10:36:27 +0100

    fix: :bug: Renamed response type Error
    Renamed error type because this type already exist and overwrite the custom attributes

--- types/response.d.ts
+++ types/response.d.ts
@@ -1,4 +1,4 @@
-type Error = {
+type CustomError = {
     domain: String; // Unique identifier for the service raising this error. 
     reason: String; // {"reason": "ResourceNotFoundException"} Unique identifier for this error. Different from the error.code property in that this is not an http response code.
     message: String // Human readable message
@@ -29,7 +29,7 @@ type ApiResponse = {
     error: {
         code: String;
         message: String; //A human readable message providing more details about the error. If there are multiple errors, message will be the message for the first error.
-        errors?: Error[]
+        errors?: CustomError[]
     };
     data?: never;
 }
