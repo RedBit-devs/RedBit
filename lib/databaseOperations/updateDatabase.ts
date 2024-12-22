9e3e1176e41e23dda418f5f7c76111a88468ebf2
413ksz <developer413ksz@gmail.com>
2024-12-22 17:43:58 +0100

    feat: :sparkles: Added update generic method
    Added update generic method and error handling

---
+++ lib/databaseOperations/updateDatabase.ts
@@ -0,0 +1,71 @@
+import { Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+
+const updateRecord = async <T>(
+  prisma: PrismaClient,
+  table: string,
+  data: T,
+  id : string,
+  apiResponse: ApiResponse
+): Promise<ApiResponse> => {
+  try {
+    await prisma[table].update({
+      where: {
+        id: id,
+      },
+      data: data,
+    });
+  } catch (error) {
+    console.log(error.message)
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        apiResponse.error = {
+          code: "400",
+          message: `The unique constraint failed on the ${table} table with the following values: ${error.meta?.target}`,
+          errors: [
+            {
+              domain: "Prisma",
+              reason: "UniqueConstraintFailed",
+              message: `The unique constraint failed on the ${table} table with the following values: ${error.meta?.target}`,
+            },
+          ],
+        };
+      }
+    }
+    else if (error instanceof Prisma.PrismaClientValidationError) {
+        apiResponse.error =  {
+            code: "400",
+            message: `Something was not in the correct format`,
+            errors: [
+              {
+                domain: "Prisma",
+                reason: "ValidationError",
+                message: `Something was not in the correct format ${error.cause}`
+              }
+            ]
+        }
+    }
+    else {
+        apiResponse.error = {
+            code: "500",
+            message: `An unknown error occurred: ${error.message}`,
+            errors: [
+              {
+                domain: "Prisma",
+                reason: "UnknownError",
+                message: "An unexpected error occurred on the server.",
+              },
+            ],
+          };
+    }
+    return apiResponse;
+  }
+  apiResponse.data = {
+    fields: prisma[table].fields,
+    totalItems: 1,
+    items: [data],
+  };
+  return apiResponse;
+};
+
+export default updateRecord;
--- server/api/user/test.ts
+++ server/api/user/test.ts
@@ -1,9 +1,11 @@
 import prisma from "~/lib/prisma";
 import deleteRecord from "~/lib/databaseOperations/deleteDatabase";
 import readRecord from "~/lib/databaseOperations/readDatabase";
+import updateRecord from "~/lib/databaseOperations/updateDatabase";
 
 export default defineEventHandler(async (event) => {
   const apiResponse = {} as ApiResponse
-  const apiResponse2 = await readRecord(prisma, 'User', "cm4zmjxrn00011kry4251ckwy", apiResponse)
+  const user = await readBody(event) as User
+  const apiResponse2 = await updateRecord(prisma, 'User', user,"cm4zmliib0001zov733twcpbk", apiResponse)
   return apiResponse2
 })
