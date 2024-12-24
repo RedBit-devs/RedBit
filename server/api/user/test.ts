56d68d08a3d402580d7def4d4a023ed339e75f53
413ksz <developer413ksz@gmail.com>
2024-12-24 11:28:19 +0100

    chore: :fire: Removed test file
    Removed test file passed all test CRUD futher testing require more real test casses and api calls

--- server/api/user/test.ts
+++
@@ -1,7 +0,0 @@
-import updateRecord from "~/lib/prisma/databaseOperations/updateRecord"
-export default defineEventHandler(async (event) => {
-  const user = await readBody(event)
-  const apiResponse = {} as ApiResponse
-
-  await updateRecord("user", user, user.id, apiResponse);
-})
