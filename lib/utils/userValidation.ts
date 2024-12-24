1b46d16c75161935ce2faa9008f53b93886dd8b5
413ksz <developer413ksz@gmail.com>
2024-12-24 11:56:00 +0100

    feat: :sparkles: Added validate user function
    Added validate user function that validates the password,email,names,username and return a boolen and set the api response

--- lib/utils/userValidation.ts
+++ lib/utils/userValidation.ts
@@ -9,16 +9,16 @@ import bcrypt from "bcrypt";
  * @returns {Promise<string>} - A promise that resolves to the hashed password.
  */
 const hashPassword = async (password: string): Promise<string> => {
-    const saltRounds = 12;
-    const salt = await bcrypt.genSalt(saltRounds);
-    try {
-      const hash = await bcrypt.hash(password, salt);
-      await hash.toString();
-      return hash;
-    } catch (e) {
-      throw e;
-    }
-  };
+  const saltRounds = 12;
+  const salt = await bcrypt.genSalt(saltRounds);
+  try {
+    const hash = await bcrypt.hash(password, salt);
+    await hash.toString();
+    return hash;
+  } catch (e) {
+    throw e;
+  }
+};
 /**
  * Validates the given password.
  *
@@ -33,14 +33,11 @@ const hashPassword = async (password: string): Promise<string> => {
  * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating if the password is valid.
  */
 const isPasswordValid = async (password: string): Promise<boolean> => {
-    const validPasswordPattern: RegExp =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-    const validPasswordCheck = validPasswordPattern.test(password);
-    return validPasswordCheck;
-  };
-  
-
-
+  const validPasswordPattern: RegExp =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  const validPasswordCheck = validPasswordPattern.test(password);
+  return validPasswordCheck;
+};
 
 /**
  * Validates the given email address.
@@ -54,49 +51,111 @@ const isPasswordValid = async (password: string): Promise<boolean> => {
  * @returns {Promise<boolean>} - A promise resolving to a boolean indicating if the email is valid.
  */
 const isEmailValid = async (email: string): Promise<boolean> => {
-    const validEmailPattern: RegExp =
-      /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const validEmailCheck = validEmailPattern.test(email);
-    return validEmailCheck;
-  };
-  
-  /**
-   * Validates the given username.
-   *
-   * The username must match the following requirements:
-   * - At least 3 characters long
-   * - At most 32 characters long
-   * - Contains only letters (both uppercase and lowercase), numbers, and underscore
-   *
-   * @param {string} username - The username to be validated.
-   * @returns {Promise<boolean>} - A promise resolving to a boolean indicating if the username is valid.
-   */
-  const isUsernameValid = async (username: string): Promise<boolean> => {
-    const validUsernamePattern: RegExp = /^[a-zA-Z0-9_]{3,32}/;
-    const validUsernameCheck = validUsernamePattern.test(username);
-    return validUsernameCheck;
-  };
-  /**
-   * Validates the given name.
-   *
-   * The name must match the following requirements:
-   * - At least 3 characters long
-   * - At most 35 characters long
-   * - Contains only letters (both uppercase and lowercase)
-   *
-   * @param {string} name - The name to be validated.
-   * @returns {Promise<boolean>} - A promise resolving to a boolean indicating if the name is valid.
-   */
-  const isNameValid = async (name: string): Promise<boolean> => {
-    const validNamePattern: RegExp = /^[a-zA-Z]{3,35}/;
-    const validNameCheck = validNamePattern.test(name);
-    return validNameCheck;
-  };
-  
+  const validEmailPattern: RegExp =
+    /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const validEmailCheck = validEmailPattern.test(email);
+  return validEmailCheck;
+};
+
+/**
+ * Validates the given username.
+ *
+ * The username must match the following requirements:
+ * - At least 3 characters long
+ * - At most 32 characters long
+ * - Contains only letters (both uppercase and lowercase), numbers, and underscore
+ *
+ * @param {string} username - The username to be validated.
+ * @returns {Promise<boolean>} - A promise resolving to a boolean indicating if the username is valid.
+ */
+const isUsernameValid = async (username: string): Promise<boolean> => {
+  const validUsernamePattern: RegExp = /^[a-zA-Z0-9_]{3,32}/;
+  const validUsernameCheck = validUsernamePattern.test(username);
+  return validUsernameCheck;
+};
+/**
+ * Validates the given name.
+ *
+ * The name must match the following requirements:
+ * - At least 3 characters long
+ * - At most 35 characters long
+ * - Contains only letters (both uppercase and lowercase)
+ *
+ * @param {string} name - The name to be validated.
+ * @returns {Promise<boolean>} - A promise resolving to a boolean indicating if the name is valid.
+ */
+const isNameValid = async (name: string): Promise<boolean> => {
+  const validNamePattern: RegExp = /^[a-zA-Z]{3,35}/;
+  const validNameCheck = validNamePattern.test(name);
+  return validNameCheck;
+};
+
+const userValidation = async (
+  newUser: User,
+  apiResponse: ApiResponse
+): Promise<boolean> => {
+  let validationError = false;
+  if (!(await isPasswordValid(newUser.password))) {
+    validationError = true;
+    apiResponse.error.errors.push({
+      domain: "users",
+      reason: "PasswordValidationFailed",
+      message:
+        "Password is not valid it must contain at least 8 characters, one uppercase letter, one lowercase letter, one number and one special character",
+    },);
+  }
+  if (!(await isEmailValid(newUser.email))) {
+    validationError = true;
+    apiResponse.error.errors.push(
+      {
+        domain: "users",
+        reason: "EmailValidationFailed",
+        message: "The provided email is not valid",
+      },
+    );
+  }
+  if (!(await isUsernameValid(newUser.username))) {
+    validationError = true;
+    apiResponse.error.errors.push(
+      {
+        domain: "users",
+        reason: "usernameValidationFailed",
+        message: "Username is not in the correct format it must be between 3 and 32 characters long and can only contain letters, numbers and underscores",
+      },
+    );
+  }
+  if (!(await isNameValid(newUser.first_name))) {
+    validationError = true;
+    apiResponse.error.errors.push(
+      {
+        domain: "users",
+        reason: "NameValidationFailed",
+        message: "First name is not in the correct format it must be between 3 and 35 characters long and can only contain letters",
+      },
+    );
+
+  }
+  if (!(await isNameValid(newUser.last_name))) {
+    validationError = true;
+    apiResponse.error.errors.push(        {
+      domain: "users",
+      reason: "NameValidationFailed",
+      message: "Last name is not in the correct format it must be between 3 and 35 characters long and can only contain letters",
+    },);
+
+  }
+  if (validationError) {
+    apiResponse.error.code = "400";
+    apiResponse.error.message = "User Validation failed";
+  }
+  return validationError;
+};
+
 export default {
-    hashPassword,
-    isPasswordValid,
-    isEmailValid,
-    isUsernameValid,
-    isNameValid
-}
+  hashPassword,
+  isPasswordValid,
+  isEmailValid,
+  isUsernameValid,
+  isNameValid,
+  userValidation,
+};
--- server/api/user/.put.ts
+++ server/api/user/.put.ts
@@ -1,5 +1,5 @@
 import createRecord from "~/lib/prisma/databaseOperations/createRecord";
-import userValidation from "~/lib/utils/userValidation";
+import ValidateUser from "~/lib/utils/userValidation";
 
 
 export default defineEventHandler(async (event) => {
@@ -16,97 +16,15 @@ export default defineEventHandler(async (event) => {
     password: newUser.password,
   };
 
-  if (!(await userValidation.isPasswordValid(newUser.password))) {
+  if((await ValidateUser.userValidation(newUser,apiResponse)))
+  {
     setResponseStatus(event, 400);
-    apiResponse.error = {
-      code: "400",
-      message:
-        "Password is not valid it must contain at least 8 characters, one uppercase letter, one lowercase letter, one number and one special character",
-      errors: [
-        {
-          domain: "users",
-          reason: "PasswordValidationFailed",
-          message: "The provided password is not valid",
-        },
-      ],
-    };
-    return {
-      apiResponse,
-    };
-  }
-  if (!(await userValidation.isEmailValid(newUser.email))) {
-    setResponseStatus(event, 400);
-    apiResponse.error = {
-      code: "400",
-      message: "Email is not in the correct format",
-      errors: [
-        {
-          domain: "users",
-          reason: "EmailValidationFailed",
-          message: "The provided email is not valid",
-        },
-      ],
-    };
-    return {
-      apiResponse,
-    };
-  }
-  if (!(await userValidation.isUsernameValid(newUser.username))) {
-    setResponseStatus(event, 400);
-    apiResponse.error = {
-      code: "400",
-      message:
-        "Username is not in the correct format it must be between 3 and 32 characters long and can only contain letters, numbers and underscores",
-      errors: [
-        {
-          domain: "users",
-          reason: "usernameValidationFailed",
-          message: "The provided username is not valid",
-        },
-      ],
-    };
-    return {
-      apiResponse,
-    };
-  }
-  if (!(await userValidation.isNameValid(newUser.first_name))) {
-    setResponseStatus(event, 400);
-    apiResponse.error = {
-      code: "400",
-      message:
-        "First name is not in the correct format it must be between 3 and 35 characters long and can only contain letters",
-      errors: [
-        {
-          domain: "users",
-          reason: "NameValidationFailed",
-          message: "The provided first name is not valid",
-        },
-      ],
-    };
-    return {
-      apiResponse,
-    };
-  }
-  if (!(await userValidation.isNameValid(newUser.last_name))) {
-    setResponseStatus(event, 400);
-    apiResponse.error = {
-      code: "400",
-      message:
-        "Last name is not in the correct format it must be between 3 and 35 characters long and can only contain letters",
-      errors: [
-        {
-          domain: "users",
-          reason: "NameValidationFailed",
-          message: "The provided last name is not valid",
-        },
-      ],
-    };
     return {
       apiResponse,
     };
   }
   newUser.birthdate = new Date(newUser.birthdate);
-  newUser.password = await userValidation.hashPassword(newUser.password);
+  newUser.password = await ValidateUser.hashPassword(newUser.password);
   await createRecord("user", newUser, apiResponse);
   if (apiResponse.error) {
     setResponseStatus(event, 400);
