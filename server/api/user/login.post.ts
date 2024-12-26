22c705b36e2daae4be035a4dc9d00ba88e859eb0
PetiDev <peteristvanh@gmail.com>
2024-12-26 15:58:24 +0100

    feat: :zap: Updated token data

--- server/api/user/login.post.ts
+++ server/api/user/login.post.ts
@@ -104,7 +104,10 @@ export default eventHandler(async (event) => {
             {
                 token: `Bearer ${jwt.sign(
                     {
-                        userId: userCredentials?.id,
+                        user:{
+                            id: userCredentials?.id,
+                            email: userCredentials?.email
+                        },
                     },
                     config.JWT_SECRET,
                     {
