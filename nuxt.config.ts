36c87b5017e0b9a21b212c658607e1dbda259acd
PetiDev <peteristvanh@gmail.com>
2024-12-26 15:02:10 +0100

    feat: :wrench: added "imports" for JWT variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  runtimeConfig:{
+    JWT_SECRET:process.env.JWT_SECRET,
+    JWT_EXP_TIME:process.env.JWT_EXP_TIME
+  },
   css: [
     '~/assets/css/main.css',
     '~/assets/css/fonts.css'
