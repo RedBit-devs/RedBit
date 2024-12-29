0bcb84ed75d9fc4885267c16221a69463ac857da
413ksz <developer413ksz@gmail.com>
2024-12-29 12:34:06 +0100

    chore: :construction: Added basic layout to apiresponse handling
    Added basic layout to apiresponse handling

--- server/utils/apiResponseHandler.ts
+++ server/utils/apiResponseHandler.ts
@@ -1,6 +1,30 @@
 
 
+const ApiResponseHandler = (event: any, customErrorMessages: Error[]) => {
+  const apiResponse = event.context.apiResponse;
+  apiResponse.error = {
+    
+  }
+  if (customErrorMessages.length == 0) 
+  {
+    return
+  }
+  if (customErrorMessages[0].name.startsWith("Prisma")) {
+    //future prisma api response handling
+  }else if(customErrorMessages[0].name.startsWith("User")){
 
-const ApiResponseHandler = (event: any) => {
-  
+  } else{
+    apiResponse.error = {
+      code: "500",
+      message: `An unknown error occurred`,
+      errors: [
+        {
+          domain: event.context.apiResponse.context,
+          reason: "UnknownError",
+          message: "An unknown error occurred",
+        },
+      ],
+    };
+  }
+  event.context.apiResponse = apiResponse
 }
