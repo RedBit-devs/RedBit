f621edbb4ee4b2f72305c140fa54b30f7d314582
PetiDev <peteristvanh@gmail.com>
2025-01-08 20:07:45 +0100

    feat: :sparkles: Verify user email api route

--- server/api/user/verifyEmail.get.ts
+++ server/api/user/verifyEmail.get.ts
@@ -1,16 +1,81 @@
+//URL template
+//http://localhost:3000/api/user/verifyEmail?id={userId}&email={email}
+
+import prisma from "~/lib/prisma";
+
 export default defineEventHandler(async (event) => {
-    
-    const { userId, email } = getQuery(event)
-    
+
+    let { id, email }: { id: string, email: string } = getQuery(event)
+
     const apiResponse = {} as ApiResponse;
-    apiResponse.context = "UserCreate";
+    apiResponse.context = "verifyEmail";
     apiResponse.method = "GET";
-    
-    //dont know if i should use the params in this case
     apiResponse.params = {
-        userId,
-        email
+        id: id,
+        email: email
     };
-    
-    return "Hello people"
+
+    const errorMessages: CustomErrorMessage[] = []
+    event.context.customErrorMessages = errorMessages;
+    event.context.apiResponse = apiResponse;
+
+    if (!id || !email) {
+        errorMessages.push({
+            espectedFrom: "User",
+            reason: "MissingParameters"
+        })
+
+        apiResponseHandler(event, errorMessages)
+        if (apiResponse.error) {
+            return apiResponse
+        }
+    }
+
+
+    const findEmailResponse = await prisma.user.findFirst({
+        where: { id },
+        select: {
+            email: true
+        }
+    })
+
+    if (!findEmailResponse) {
+        errorMessages.push({
+            espectedFrom: "Prisma",
+            reason: "IdentifierNotFound",
+            table: "User",
+            target: id
+        })
+    }
+
+    if (findEmailResponse && findEmailResponse.email != email) {
+        errorMessages.push({
+            espectedFrom: "User",
+            reason: "DataDontMatch"
+        })
+    }
+
+    apiResponseHandler(event, errorMessages)
+    if (apiResponse.error) {
+        return apiResponse
+    }
+
+    const response = await prisma.user.update({
+        where: { id },
+        data: {
+            email_verified: true
+        },
+        select: {
+            email_verified: true
+        }
+    })
+
+    apiResponseHandler(event, errorMessages, {
+        totalItems: 1,
+        items: [response]
+    })
+
+    return apiResponse
+    // Hello there
+    //https://pm1.aminoapps.com/6525/10c214eab1fbae69c7b0d49df0aa4fd715e1a36d_hq.jpg
 })
