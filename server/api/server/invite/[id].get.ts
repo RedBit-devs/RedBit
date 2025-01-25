620ea802a1c10701b41a5b9c983f663c4041cac4
PetiDev <peteristvanh@gmail.com>
2025-01-25 18:12:47 +0100

    fix: :bug: paramsCheck and unique constrait

--- server/api/server/invite/[id].get.ts
+++ server/api/server/invite/[id].get.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import prisma from "~/lib/prisma";
 
 export default defineEventHandler(async (event) => {
@@ -13,7 +14,7 @@ export default defineEventHandler(async (event) => {
         // 401 == "unauthorized"
         throw createError({ statusCode: 401 })
     }
-    if (paramsCheck(event)) {
+    if (paramsCheck(apiResponse.params)) {
         throw createError({ statusCode: 400, statusMessage: "Not all required parameters where sspecified" })
     }
 
@@ -27,17 +28,29 @@ export default defineEventHandler(async (event) => {
         throw createError({ statusCode: 404, statusMessage: "No invite link was found with the given id" })
     }
 
-    if (inviteDbResponse.created_at.getTime() + inviteDbResponse.lifetime > new Date().getTime()) {
+    if (inviteDbResponse.created_at.getTime() + inviteDbResponse.lifetime < new Date().getTime()) {
         throw createError({ statusCode: 400, statusMessage: "Invite link expired" })
     }
 
-    const dbResponse = await prisma.server_User_Connect.create({
-        data: {
-            server_id: inviteDbResponse.server_id,
-            user_id: event.context.auth.user.id
-        }
-    })
 
+    let dbResponse;
+    //TODO When migrating to the new API handler this should be changed to the createRecord function
+    try {
+        dbResponse = await prisma.server_User_Connect.create({
+            data: {
+                server_id: inviteDbResponse.server_id,
+                user_id: event.context.auth.user.id
+            }
+        })
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            
+            if (error.code === "P2002") {
+                throw createError({statusCode: 500, statusMessage: "unique constrait failed"})
+        }
+            
+        }
+    }
     apiResponse.data = {
         totalItems: 1,
         fields: prisma.server_User_Connect.fields,
