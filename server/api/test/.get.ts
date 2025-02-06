374481cf54a3352900e8635bf4a047c2364beb19
413ksz <developer413ksz@gmail.com>
2025-02-06 19:11:29 +0100

    feat: :sparkles: Send email tem,plates with renderer
    Send email tem,plates with renderer

--- server/api/test/.get.ts
+++ server/api/test/.get.ts
@@ -1,9 +1,16 @@
+import Handlebars from "handlebars";
+
+
 export default defineEventHandler(async (event) => {
+  console.log("here");
   const { sendMail } = useNodeMailer()
+
+  const template = Handlebars.compile('{{ name }}');
+  const html = template({ name: 'myself' });
   try {
-    sendMail({ subject: 'test', text: 'test from app', to: 'developer413ksz@gmail.com' })
+    sendMail({ subject: 'me', html, to: 'developer413ksz@gmail.com'})
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
   return {message:"email sent"}
 })
