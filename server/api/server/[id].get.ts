9b43929bf1fd0ea4ea9833fe0239bc8d230485d0
413ksz <developer413ksz@gmail.com>
2025-02-06 17:07:35 +0100

    feat: :sparkles: Added check for membership for server and check for ownership
    Added check for membership for server and check for ownership

--- server/api/server/[id].get.ts
+++ server/api/server/[id].get.ts
@@ -1,82 +1,90 @@
 import prisma from "~/lib/prisma";
 import readRecord from "~/lib/prisma/databaseOperations/readRecord";
-import { type CustomErrorMessage, errorExpectedFroms, errorReasons } from "~/types/customErrorMessage";
+import {
+  type CustomErrorMessage,
+  errorExpectedFroms,
+  errorReasons,
+} from "~/types/customErrorMessage";
 
-export default defineEventHandler(async (event) =>{
-    const customErrorMessages: CustomErrorMessage[] = [];
-    const apiResponse = {} as ApiResponse;
-  
-    apiResponse.context = "Server/Get";
-    apiResponse.method = "GET";
-    event.context.apiResponse = apiResponse;
+export default defineEventHandler(async (event) => {
+  const customErrorMessages: CustomErrorMessage[] = [];
+  const apiResponse = {} as ApiResponse;
 
-    if (!event.context.auth) {
-        customErrorMessages.push(
-          {
-            expectedFrom: errorExpectedFroms.Server,
-            reason: errorReasons.Unauthorized,
-          }
-        );
-        const {errors} = apiResponseHandler(event, customErrorMessages);
-        throw createError(errors);  
+  apiResponse.context = "Server/Get";
+  apiResponse.method = "GET";
+  event.context.apiResponse = apiResponse;
+
+  if (!event.context.auth) {
+    customErrorMessages.push({
+      expectedFrom: errorExpectedFroms.Server,
+      reason: errorReasons.Unauthorized,
+    });
+    const { errors } = apiResponseHandler(event, customErrorMessages);
+    throw createError(errors);
+  }
+
+  let serverId = getRouterParam(event, "id");
+  const userId = event.context.auth.user.id;
+  if (!serverId) {
+    serverId = "";
+  }
+  apiResponse.params = {
+    serverId: serverId,
+    userId: userId
+  };
+  event.context.apiResponse = apiResponse;
+
+  if (paramsCheck(apiResponse.params)) {
+    customErrorMessages.push({
+      expectedFrom: errorExpectedFroms.Server,
+      reason: errorReasons.MissingParameters,
+    });
+    const { errors } = apiResponseHandler(event, customErrorMessages);
+    throw createError(errors);
+  }
+
+  const data = await readRecord("server", serverId, customErrorMessages);
+  const { errors } = apiResponseHandler(event, customErrorMessages, data);
+  if (customErrorMessages.length > 0) {
+    throw createError(errors);
+  }
+
+  if (!data) {
+    customErrorMessages.push({
+      expectedFrom: errorExpectedFroms.Prisma,
+      reason: errorReasons.NoDatabaseResponse,
+    });
+    const { errors } = apiResponseHandler(event, customErrorMessages);
+    throw createError(errors);
+  }
+
+  if (data.visibility === "private") {
+    const owner = await prisma.server.findFirst({
+      where: {
+        id: serverId,
+        owner_id: userId
       }
-
-    let serverId = getRouterParam(event, 'id');
-    if(!serverId){
-      serverId = ""
+    })
+    if(owner) {
+      return apiResponse;
     }
-    apiResponse.params = {
-      id: serverId,
-    };
-    event.context.apiResponse = apiResponse;
 
-    if (paramsCheck(apiResponse.params)) {
-        customErrorMessages.push(
-          {
-            expectedFrom: errorExpectedFroms.Server,
-            reason: errorReasons.MissingParameters,
-          }
-        );
-        const {errors} = apiResponseHandler(event, customErrorMessages);
-        throw createError(errors);  
-      }
-    
-      const data = await readRecord("server", serverId, customErrorMessages);
-      const {errors} = apiResponseHandler(event, customErrorMessages,data);
-      if (customErrorMessages.length > 0) {
-        throw createError(errors);
-      }
+    const member = await prisma.server_User_Connect.findFirst({
+      where: {
+        server_id: serverId,
+        user_id: userId,
+      },
+    });
+    if (member) {
+      return apiResponse;
+    }
 
-      if(!data){
-        customErrorMessages.push(
-          {
-            expectedFrom: errorExpectedFroms.Prisma,
-            reason: errorReasons.NoDatabaseResponse,
-          }
-        )
-        const {errors} = apiResponseHandler(event, customErrorMessages);
-        throw createError(errors);
-      }
-
-      if(data.visibility == "private")
-        {
-          const valami = await prisma.server_User_Connect.findMany(
-          {
-            where: { 
-             server_id = serverId, 
-            }
-          }
-          )
-          customErrorMessages.push(
-            {
-              expectedFrom: errorExpectedFroms.Server,
-              reason: errorReasons.PrivateServerContent
-            }
-          )
-          const {errors} = apiResponseHandler(event, customErrorMessages);
-          throw createError(errors);
-        }
-
-
-      return apiResponse
-})
+    customErrorMessages.push({
+      expectedFrom: errorExpectedFroms.Server,
+      reason: errorReasons.PrivateServerContent,
+    }) 
+    const { errors } = apiResponseHandler(event, customErrorMessages);
+    throw createError(errors);
+  }    
+  return apiResponse;
+});
