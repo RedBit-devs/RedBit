2ee25de3804ab45ff9e3a4b378125afdb07c51ae
Kevin <kevinszigetvari40@gmail.com>
2025-02-17 20:18:08 +0100

    feat: :memo: Created the discover card for the servers
    Created the discover card for the servers

---
+++ components/DiscoverInputField.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <div class="close">
+            <Icon name="mdi:close" size="150%" class="btn primary" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
--- pages/discoverServerPage.vue
+++ pages/discoverServerPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-
+        <DiscoverInputField />
     </div>
 </template>
 
