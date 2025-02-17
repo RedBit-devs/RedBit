fcd397ca0266510be3c5107e27fc56f116e6795c
PetiDev <peteristvanh@gmail.com>
2025-02-17 21:21:40 +0100

    refactor: :mute: removed console.log

--- middleware/protected.ts
+++ middleware/protected.ts
@@ -3,7 +3,6 @@ export default defineNuxtRouteMiddleware((to, from) => {
     const { token } = useToken()
 
     if (!token.value) {
-        console.log("asd");
         return navigateTo("/loginPage")
     }
 
