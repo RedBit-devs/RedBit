62f7855de1af8a7b6a558feac8df4d740f2cc39d
Horváth Péter István <54474482+PetiDev@users.noreply.github.com>
2025-02-21 07:49:28 +0000

    test: testing subscribe and unsbscribe

--- pages/ize.vue
+++ pages/ize.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    
+    <div ref="chatRef" id="chat">
+
+    </div>
+    <button class="btn primary" @click="subscribeToTestTopic">
+        Sub to testTopic
+    </button>
+    <button class="btn secondary" @click="unsubscribeToTestTopic">
+        Un Sub to testTopic
+    </button>
   </div>
 </template>
 
@@ -9,12 +17,38 @@
 import { useWebSocket } from '@vueuse/core'
 
 
+const chatRef = ref(null);
+
 const { status, data, send, open, close } = useWebSocket('ws://localhost:3000/_ws', {
     onMessage(ws, event){
-        console.log(event);
+      const {type, data} = JSON.parse(event.data)
+        switch (type) {
+          case "welcome":
+            alert(data)
+            break;
+          case "message":
+            chatRef.value.innerText += data;
+            break;
+          case "system":
+            chatRef.value.innerText += data;
+            break;
+
+          default:
+          console.log(event.data);
+            break;
+        }
         
     }
 })
+
+const subscribeToTestTopic = () => {
+  console.log("asd");
+  
+  send(JSON.stringify({type:"subscribe", data:"testTopic"}))
+}
+const unsubscribeToTestTopic = () => {
+  send(JSON.stringify({type:"unsubscribe", data:"testTopic"}))
+}
 
 console.log(data);
 
--- server/routes/_ws.ts
+++ server/routes/_ws.ts
@@ -4,21 +4,38 @@ import { Peer, Message } from 'crossws';
 export default defineWebSocketHandler({
     open(peer: Peer) {
         // Send welcome to the new client
-        peer.send(JSON.stringify({ type: 'welcome', message: 'Welcome to the server!' }));
+        peer.send(JSON.stringify({ type: 'welcome', data: 'Welcome to the server!\n' }));
 
         // Join new client to the "matrix" channel
         peer.subscribe("matrix");
 
         // Notify every other connected client
-        peer.publish("matrix", `[system] ${peer.toString()} has joined the Matrix!`);
-        console.log("asdasd");
+        peer.publish("matrix", JSON.stringify({type: "system", data: `[system] ${peer.toString()} has joined the Matrix!\n`}));
+        console.log(`open: ${peer.toString()}`);
         
     },
 
-    message(peer: Peer, message: Message) { },
+    message(peer: Peer, message: Message) {
+        const {type, data} = JSON.parse(message.toString())
+        switch (type) {
+            case "subscribe":
+                console.log(type);
+                
+                peer.subscribe(data)
+                peer.publish(data, JSON.stringify({type: "system", data: `${peer.toString()} joined ${data}\n`}))
+                break;
+            case "unsubscribe":
+                peer.publish(data, JSON.stringify({type: "system", data: `${peer.toString()} left ${data}\n`}))
+                peer.unsubscribe(data)
+                break;
+        
+            default:
+                break;
+        }
+     },
 
     close(peer: Peer) {
-        console.log("close asdasd");
+        console.log(`close: ${peer.toString()}`);
      }
 });
 
