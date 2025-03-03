78d54112d575435b1b6dd430bc6876e0c42f98a3
Kevin <kevinszigetvari40@gmail.com>
2025-03-03 20:57:25 +0100

    style: :lipstick: Created the first part of the settings page
    Created the first part of the settings page

--- pages/chatSettingsPage.vue
+++ pages/chatSettingsPage.vue
@@ -1,16 +1,86 @@
 <template>
-  <div>
-    <h1>Alma</h1>
+  <div class="content">
+    <div class="close ">
+      <NuxtLink to="/chatPage">
+        <Icon name="mdi:close" size="550%" class="btn primary" />
+      </NuxtLink>
+    </div>
+    <h1 class="title text-title">Chat settings</h1>
+    <div class="mainInfo">
+      <img src="../img/probalogo.png" alt="">
+
+      <div class="data">
+        <h2>Server name: Kacsa sziget</h2>
+        <h2>Owner: Kicsi Kacsa</h2>
+        <h2>Created at: 1.01.01</h2>
+      </div>
+    </div>
+    <div class="members">
+      <h2 class="text-title ">Members of the chat</h2>
+      <div class="cardContainer">
+        <ChatCard />
+        <ChatCard />
+        <ChatCard />
+        <ChatCard />
+        <ChatCard />
+      </div>
+    </div>
+
+
   </div>
 </template>
 
 <script setup>
-  definePageMeta({
-    layout: false
-  })
+definePageMeta({
+  layout: false
+})
 
 </script>
 
 <style scoped>
+.content {
+  display: grid;
+  grid-template-rows: 1fr;
+  gap: 3rem;
+}
 
+.close {
+  text-align: right;
+}
+
+.title {
+  text-align: center;
+  text-transform: capitalize;
+}
+
+.mainInfo {
+  display: flex;
+  gap: 3rem;
+  justify-content: center;
+}
+
+.mainInfo img {
+  border-radius: 50%;
+  height: 20rem;
+}
+
+.data {
+  gap: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.data h2 {
+  background-color: var(--clr-ui-primary);
+  border-radius: var(--border-rounded);
+  padding: 1rem;
+
+}
+
+.members h2 {
+  text-align: center;
+  text-transform: capitalize;
+}
 </style>
