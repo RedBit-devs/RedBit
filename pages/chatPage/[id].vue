3c0cf24139b0494d1fd2c77f3892b0334dc3da38
PetiDev <peteristvanh@gmail.com>
2025-03-03 23:13:24 +0100

    refactor: :label: Implemented types

--- pages/chatPage/[id].vue
+++ pages/chatPage/[id].vue
@@ -1,98 +1,103 @@
 <template>
-    <div class="content">
-        <ChatFieldNavbar />
-        <div id="chat">
-            <ChatMessage v-for="msg in chatRef" :author-image="msg.author.picture" :author-name="msg.author.name" :message="msg.text"/>
-        </div>
-        <ChatInputFiled :sendButton="sendMessage" id="input" />
-        <div>
-            <button class="btn secondary" @click="sendMessage">
-                Send Test Message
-            </button>
-            
-        </div>
-            
-            <div  class="toaster">
-                <Toast v-for="msg in toastRef" class="warn" :content="msg.data"  :title="String(msg.type)"/>
-            </div>
-        
+  <div class="content">
+    <ChatFieldNavbar />
+    <div id="chat">
+      <ChatMessage v-for="msg in chatRef" :author-image="msg.author.picture" :author-name="msg.author.name"
+        :message="msg.data.text" />
     </div>
+    <ChatInputFiled :sendButton="sendMessage" id="input" />
+    <div>
+      <button class="btn secondary" @click="sendMessage">
+        Send Test Message
+      </button>
+
+    </div>
+
+    <div class="toaster">
+      <Toast v-for="msg in toastRef" class="warn" :content="msg.data.text" :title="msg.data.header" />
+    </div>
+
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { changeTopicMode, type changeTopicMessage, type ClientSocketMessage, type ServerSocketMessage, type textMessage, type toastMessage } from '~/types/websocket';
+
 definePageMeta({
-    layout: 'chat'
+  layout: 'chat'
 })
 
 
-import { SocketMessageMode, SocketMessageType, type socketMessage, type SocketMessage } from '~/types/websocket';
-
-
+const { getToken } = useToken()
 const route = useRoute()
-const chatRef = ref<socketMessage[]>([]);
-const toastRef = ref<SocketMessage<any>[]>([]);
+const chatRef = ref<ServerSocketMessage<textMessage>[]>([]);
+const toastRef = ref<ServerSocketMessage<toastMessage>[]>([]);
 
 const { data, send } = useWebSocket('ws://localhost:3000/_ws', {
   autoReconnect: true,
-    onMessage(ws, event){
-      const {type, data} = JSON.parse(event.data);
-      if (type === SocketMessageType.message) {
-        chatRef.value.push(data);
-      }else{
-        toastRef.value.push(JSON.parse(event.data));
-      }
+  onMessage(ws, event) {
+    const { author, data }: { author: { name: string, picture: string, id: string }, data: textMessage | toastMessage } = JSON.parse(event.data);
+
+    const keys = Object.keys(data)
+
+    if (keys.includes("to")) { 
+      chatRef.value.push({author, data: data as textMessage})
     }
+    if (keys.includes("header")) { 
+      toastRef.value.push({author, data: data as toastMessage})
+    }
+
+  }
 })
 
 const unsubscribeFromTopic = (id: string) => {
-  const message:SocketMessage<string> = {
-    type: SocketMessageType.topic,
-    mode: SocketMessageMode.unsubscribe,
-    data: id
+  const message: ClientSocketMessage<changeTopicMessage> = {
+    author: "",
+    data: {
+      mode: changeTopicMode.unsubscribe,
+      topic: id
+    }
   }
   send(JSON.stringify(message))
 }
-const subscribeToTopic = (id: string) => {
-  const message:SocketMessage<string> = {
-    type: SocketMessageType.topic,
-    mode: SocketMessageMode.subscribe,
-    data: id
+const subscribeToTopic = () => {
+  const message: ClientSocketMessage<changeTopicMessage> = {
+    author: "",
+    data: {
+      mode: changeTopicMode.subscribe,
+      topic: `${route.params.id}`
+    }
   }
-  
   send(JSON.stringify(message))
 }
+
+subscribeToTopic()
 
 
 const sendMessage = () => {
-  const message:SocketMessage<socketMessage> = {
-    type: SocketMessageType.message,
+  const message: ClientSocketMessage<textMessage> = {
+    author: getToken(),
     data: {
-      author: {
-        id: "asd",
-        name: "Lajos",
-        picture: "https://picsum.photos/200"
-      },
-      text: "Ez üzenet",
+      text: "szia",
       to: `${route.params.id}`
     }
   }
   send(JSON.stringify(message))
 }
 
-subscribeToTopic(`${route.params.id}`)
 
 </script>
 
 <style scoped>
 .content {
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
 }
 
 #chat {
-    height: 100%;
-    overflow-y: auto;
+  height: 100%;
+  overflow-y: auto;
 
 }
 </style>
--- server/routes/_ws.ts
+++ server/routes/_ws.ts
@@ -1,5 +1,5 @@
 import { Peer, Message } from 'crossws';
-import { type socketMessage, type SocketMessage, SocketMessageMode, SocketMessageType } from '~/types/websocket';
+import { changeTopicMode, type toastMessage, type changeTopicMessage, type ClientSocketMessage, type textMessage, type ServerSocketMessage } from '~/types/websocket';
 
 //Nem tudom hogy miért működik, de működik
 export default defineWebSocketHandler({
@@ -8,37 +8,77 @@ export default defineWebSocketHandler({
     },
 
     message(peer: Peer, message: Message) {
-        const {type, data, mode} = message.json<SocketMessage<any>>()
-        switch (type) {
-            case SocketMessageType.topic:
-                if (mode === SocketMessageMode.subscribe) {
-                    peer.subscribe(data)
-                }
-                if (mode === SocketMessageMode.unsubscribe) {
-                    peer.unsubscribe(data)                    
-                }
-                break;
-            case SocketMessageType.message:
-                const Data:socketMessage = data;
+        const { author, data } = message.json<ClientSocketMessage<textMessage | changeTopicMessage | toastMessage>>();
 
-                const message:SocketMessage<socketMessage> = {
-                    type: SocketMessageType.message,
-                    data: {
-                        author: Data.author,
-                        text: Data.text,
-                        to: Data.to
-                    }
-                }
+        // TODO After user is able to login to chat
+        // if (!author) {
+        //     const message: ServerSocketMessage<toastMessage> = {
+        //         author: {
+        //             id: "",
+        //             name: "Server",
+        //             picture: ""
+        //         },
+        //         data: {
+        //             header: "Unauthorized",
+        //             text: "No valid access token provided"
+        //         }
+        //     }
 
-                peer.publish(Data.to, JSON.stringify(message))
-                break;
-        
-            default:
-                break;
+        //     peer.send(JSON.stringify(message))
+        //     return
+        // }
+
+        //TODO JWTValidate once its merged
+        const Author = {
+            name: "dunno",
+            id: "meow",
+            picture: "nop"
         }
-     },
+
+        const keys = Object.keys(data)
+
+        if (keys.includes("topic")) {
+            const Data: changeTopicMessage = data as changeTopicMessage;
+
+            if (Data.mode === changeTopicMode.subscribe) {
+                peer.subscribe(Data.topic)
+            }
+            if (Data.mode === changeTopicMode.unsubscribe) {
+                peer.unsubscribe(Data.topic)
+            }
+        } else if (keys.includes("to")) {
+            const Data: textMessage = data as textMessage;
+
+            const message: ServerSocketMessage<textMessage> = {
+                author: Author,
+                data: Data
+            }
+
+            peer.publish(Data.to, JSON.stringify(message))
+            peer.send(JSON.stringify(message))
+
+        } else {
+            const message: ServerSocketMessage<toastMessage> = {
+                author: {
+                    id: "",
+                    name: "Server",
+                    picture: ""
+                },
+                data: {
+                    header: "Error",
+                    text: "Invalid message on socket"
+                }
+            }
+
+            peer.send(JSON.stringify(message))
+        }
+
+
+
+
+    },
 
     close(peer: Peer) {
         console.log(`close: ${peer.toString()}`);
-     }
+    }
 });
