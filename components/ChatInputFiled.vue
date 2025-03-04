7150905793471a42ad3c134c66773ec207566bd3
Horváth Péter István <54474482+PetiDev@users.noreply.github.com>
2025-03-04 12:13:03 +0000

    feat: Client side auth and true message sending

--- components/ChatInputFiled.vue
+++ components/ChatInputFiled.vue
@@ -3,14 +3,52 @@
         <button class="btn ui-primary">
             <Icon name="mdi:file" size="150%" />
         </button>
-        <input type="text" class="textInput">
-        <button class="btn ui-primary">
+        <input ref="inputRef" type="text" class="textInput">
+        <button class="btn ui-primary" @click="sendMessage">
             <Icon name="mdi:arrow-left-bottom" size="150%" />
         </button>
     </div>
 </template>
 
-<script setup>
+<script lang="ts" setup>
+const {send, route} = defineProps({
+
+    "send":{
+        type: Function,
+        default: () =>{}
+    },
+    "route":{
+        type: String,
+        default: ""
+    }
+})
+
+import { type ClientSocketMessage, type textMessage } from '~/types/websocket';
+const { getToken, getNewToken } = useToken()
+const inputRef = ref(null)
+
+const checkToken = async () => {
+  if (!getToken()) {
+    let success = await getNewToken();
+    if (!success) {
+      navigateTo('/loginpage')
+    }
+  }
+}
+
+
+const sendMessage = async () => {
+  await checkToken()
+  const message: ClientSocketMessage<textMessage> = {
+    author: getToken(),
+    data: {
+      text: inputRef.value.value,
+      to: route
+    }
+  }
+  send(JSON.stringify(message))
+}
+
 
 </script>
 
--- pages/chatPage/[id].vue
+++ pages/chatPage/[id].vue
@@ -2,13 +2,16 @@
   <div class="content">
     <ChatFieldNavbar />
     <div id="chat">
-      <ChatMessage v-for="msg in chatRef" :author-image="msg.author.picture" :author-name="msg.author.name"
+      <ChatMessage v-for="msg in chatRef" :author-image="msg.author.picture" :author-name="msg.author.username"
         :message="msg.data.text" />
     </div>
-    <ChatInputFiled :sendButton="sendMessage" id="input" />
+    <ChatInputFiled :send="send" :route="`${route.params.id}`" id="input" />
     <div>
-      <button class="btn secondary" @click="sendMessage">
-        Send Test Message
+      <button class="btn secondary" @click="clearToken">
+        Clear Token
+      </button>
+      <button class="btn secondary" @click="clearRefreshToken">
+        Clear Refresh Token
       </button>
 
     </div>
@@ -21,38 +24,51 @@
 </template>
 
 <script lang="ts" setup>
-import { changeTopicMode, type changeTopicMessage, type ClientSocketMessage, type ServerSocketMessage, type textMessage, type toastMessage } from '~/types/websocket';
+import { changeTopicMode, type changeTopicMessage, type ClientSocketMessage, type ServerSocketMessage, type textMessage, type toastMessage, type author } from '~/types/websocket';
 
 definePageMeta({
   layout: 'chat'
 })
 
 
-const { getToken } = useToken()
+const { getToken, getNewToken, clearToken, clearRefreshToken } = useToken()
 const route = useRoute()
 const chatRef = ref<ServerSocketMessage<textMessage>[]>([]);
 const toastRef = ref<ServerSocketMessage<toastMessage>[]>([]);
 
+
+const checkToken = async () => {
+  if (!getToken()) {
+    let success = await getNewToken();
+    if (!success) {
+      navigateTo('/loginpage')
+    }
+  }
+}
+
+
+
 const { data, send } = useWebSocket('/_ws', {
   autoReconnect: true,
   onMessage(ws, event) {
-    const { author, data }: { author: { name: string, picture: string, id: string }, data: textMessage | toastMessage } = JSON.parse(event.data);
+    const { author, data }: { author: author, data: textMessage | toastMessage } = JSON.parse(event.data);
 
     const keys = Object.keys(data)
 
-    if (keys.includes("to")) { 
-      chatRef.value.push({author, data: data as textMessage})
+    if (keys.includes("to")) {
+      chatRef.value.push({ author, data: data as textMessage })
     }
-    if (keys.includes("header")) { 
-      toastRef.value.push({author, data: data as toastMessage})
+    if (keys.includes("header")) {
+      toastRef.value.push({ author, data: data as toastMessage })
     }
 
   }
 })
 
 const unsubscribeFromTopic = (id: string) => {
+  checkToken()
   const message: ClientSocketMessage<changeTopicMessage> = {
-    author: "",
+    author: getToken(),
     data: {
       mode: changeTopicMode.unsubscribe,
       topic: id
@@ -61,8 +77,9 @@ const unsubscribeFromTopic = (id: string) => {
   send(JSON.stringify(message))
 }
 const subscribeToTopic = () => {
+  checkToken()
   const message: ClientSocketMessage<changeTopicMessage> = {
-    author: "",
+    author: getToken(),
     data: {
       mode: changeTopicMode.subscribe,
       topic: `${route.params.id}`
@@ -72,20 +89,6 @@ const subscribeToTopic = () => {
 }
 
 subscribeToTopic()
-
-
-const sendMessage = () => {
-  const message: ClientSocketMessage<textMessage> = {
-    author: getToken(),
-    data: {
-      text: "szia",
-      to: `${route.params.id}`
-    }
-  }
-  send(JSON.stringify(message))
-}
-
-
 </script>
 
 <style scoped>
