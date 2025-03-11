34a2f5590bbef2e83eff1c4b0ca398784d64aac4
PetiDev <peteristvanh@gmail.com>
2025-03-11 18:37:54 +0100

    fix: input accepts newlines

--- components/ChatInputFiled.vue
+++ components/ChatInputFiled.vue
@@ -3,7 +3,7 @@
     <button class="btn ui-primary">
       <Icon name="mdi:file" size="150%" />
     </button>
-    <input @keypress="handleKeypress" ref="inputRef" type="text" class="textInput">
+    <textarea @keydown="handleKeydown" @keyup="handleKeyup" ref="inputRef" type="text" class="textInput"/>
     <button class="btn ui-primary" @click="sendMessage">
       <Icon name="mdi:arrow-left-bottom" size="150%" />
     </button>
@@ -50,12 +50,26 @@ const sendMessage = async () => {
   send(JSON.stringify(message))
 }
 
-const handleKeypress = ({ keyCode }) => {
-  //on enter press
-  if (keyCode === 13) {
-    sendMessage()
+let isShiftDown = false;
+
+const handleKeydown = ({keyCode}) =>{
+  // shift is down
+  if (keyCode === 16) {
+    isShiftDown = true
+  }
+  
+}
+const handleKeyup = async ({keyCode, target}) =>{
+  // shift is down
+  if (keyCode === 16) {
+    isShiftDown = false
   }
 
+  //is enter down whitout shift held
+  if (keyCode === 13 && !isShiftDown) {
+      await sendMessage()
+      target.value = ""
+  }
 }
 
 </script>
@@ -77,5 +91,12 @@ const handleKeypress = ({ keyCode }) => {
   padding: .5rem;
   color: var(--clr-text-primary);
   width: 100%;
+  height: fit-content;
+  min-height: 3.3rem;
+  resize: vertical;
+  overflow-y: auto;
+  font-size: 1.1rem;
+  max-height: 6rem;
+  padding-top: 1rem;
 }
 </style>
