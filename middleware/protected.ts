eb3bf51b7694fcebd0d36082e497199c9b347926
PetiDev <peteristvanh@gmail.com>
2025-03-14 17:28:22 +0100

    refactor: unuset variable

--- middleware/protected.ts
+++ middleware/protected.ts
@@ -2,9 +2,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 
     const { getToken, tokenRefresh, tokenStatus } = useToken()
 
-    const token = getToken()
-
-    if (!token) {
+    if (!getToken()) {
         await tokenRefresh()
         
         if (tokenStatus.value !== "success") {
