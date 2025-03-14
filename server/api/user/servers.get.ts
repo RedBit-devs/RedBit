d8a2ee299ba2bab9203c5f6ee360ddb6b48a2865
PetiDev <peteristvanh@gmail.com>
2025-03-14 18:01:28 +0100

    feat: :sparkles: Get all server where the user is joined

---
+++ server/api/user/servers.get.ts
@@ -0,0 +1,96 @@
+import prisma from "~/lib/prisma";
+import readRecord from "~/lib/prisma/databaseOperations/readRecord";
+import { paramsCheck } from "~/shared/utils/userValidation";
+import {
+    type CustomErrorMessage,
+    errorExpectedFroms,
+    errorReasons,
+} from "~/types/customErrorMessage";
+
+export default defineEventHandler(async (event) => {
+    const customErrorMessages: CustomErrorMessage[] = [];
+    const apiResponse = {} as ApiResponse;
+
+    apiResponse.context = "User/Servers";
+    apiResponse.method = "GET";
+
+    event.context.apiResponse = apiResponse;
+
+    if (!event.context.auth) {
+        customErrorMessages.push(
+            {
+                expectedFrom: errorExpectedFroms.User,
+                reason: errorReasons.Unauthorized,
+            }
+        );
+        const { errors } = apiResponseHandler(event, customErrorMessages);
+        throw createError(errors);
+    }
+
+    const userId = event.context.auth.user.id;
+    apiResponse.params = {
+        id: userId,
+    };
+    event.context.apiResponse = apiResponse;
+
+    if (paramsCheck(apiResponse.params)) {
+        customErrorMessages.push(
+            {
+                expectedFrom: errorExpectedFroms.User,
+                reason: errorReasons.MissingParameters,
+            }
+        );
+        const { errors } = apiResponseHandler(event, customErrorMessages);
+        throw createError(errors);
+    }
+
+    const dbresponse = await prisma.server_User_Connect.findMany({
+        where: {
+            user_id: userId
+        },
+        select: {
+            Server: {
+                select: {
+                    id: true,
+                    name: true,
+                    picture: true
+                }
+            }
+        }
+    });
+    if (!dbresponse) {
+        return []
+    }
+
+
+    const data = {
+        fields: {
+            id:
+            {
+                typename: "string",
+                name: "id"
+            },
+            name:
+            {
+                typename: "string",
+                name: "name"
+            },
+            picture:
+            {
+                typename: "string",
+                name: "picture"
+            }
+        },
+        totalItems: dbresponse.length,
+        items: [
+            dbresponse.map(m => m.Server)
+        ]
+    }
+
+    const { errors } = apiResponseHandler(event, customErrorMessages,data);
+    if (customErrorMessages.length > 0) {
+        throw createError(errors);
+    }
+
+    return apiResponse;
+});
