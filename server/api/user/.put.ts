ed25b5d010fd178133e2bca2694264a1b3a31eaf
413ksz <developer413ksz@gmail.com>
2025-03-15 10:12:38 +0100

    fix: :bug: Fixed api response password return to hashed password
    Fixed api response password return to hashed password in the user.put.ts file

--- server/api/user/.put.ts
+++ server/api/user/.put.ts
@@ -10,14 +10,15 @@ export default defineEventHandler(async (event) => {
   const apiResponse = {} as ApiResponse;
   apiResponse.context = "User/Create";
   apiResponse.method = "PUT";
-  apiResponse.params = {
+  const params:User = {
     username: newUser.username,
     email: newUser.email,
     birthdate: newUser.birthdate,
     first_name: newUser.first_name,
     last_name: newUser.last_name,
     password: newUser.password,
-  };
+  }
+  apiResponse.params = params;
   const customErrorMessages: CustomErrorMessage[] = [];
   event.context.apiResponse = apiResponse;
 
@@ -28,7 +29,8 @@ export default defineEventHandler(async (event) => {
 
   newUser.birthdate = new Date(newUser.birthdate);
   newUser.password = await hashPassword(newUser.password, customErrorMessages);
-  
+  params.password = newUser.password;
+  apiResponse.params = params;
   if (customErrorMessages.length > 0) {
     const {errors} = apiResponseHandler(event, customErrorMessages);
     throw createError(errors);  
