b7935f25957b5e62a69308bc9b706c0431fca255
PetiDev <peteristvanh@gmail.com>
2025-03-16 18:26:14 +0100

    feat: :sparkles: render chatgroups

--- components/ChatSelector.vue
+++ components/ChatSelector.vue
@@ -5,15 +5,23 @@
                 <input type="text" placeholder="Search between chats">
             </div>
             <div class="chatList">
-                <NuxtLink v-for="chat in chatgroups.Chat_rooms" :key="chat?.id" :to="chat?.id">
-                <ChatCard  :name="chat?.name" />
-                </NuxtLink>
+                <dir v-for="group in chatgroups" :key="group?.id">
+                    <h4>
+                        {{group?.name}}
+                    </h4>
+                    <NuxtLink v-for="chat in group?.Chat_rooms" :key="chat?.id" :to="`/chatpage/${route.params.serverId}/${chat?.id}`">
+                        <ChatCard :name="chat?.name" />
+                    </NuxtLink>
+            </dir>
+
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
+const route = useRoute();
+
 const { chatgroups } = defineProps({
     'chatgroups': {
         default: []
