444f94627787055247b2e25cb066cd268e62b7b8
Debreczeni Alex János <developer413ksz@gmail.com>
2025-03-24 09:09:05 +0000

    chore: :construction: Added delete server api work in progress
    Added delete server api work in progress

---
+++ server/api/server/.delete.ts
@@ -0,0 +1,50 @@
+import prisma from "~/lib/prisma";
+import deleteRecord from "~/lib/prisma/databaseOperations/deleteRecord";
+import {errorExpectedFroms, errorReasons, type CustomErrorMessage } from "~/types/customErrorMessage";
+
+export default defineEventHandler(async (event) => {
+  const customErrorMessages: CustomErrorMessage[] = [];
+  const apiResponse = {} as ApiResponse;
+  apiResponse.context = "api/server";
+  apiResponse.method = "DELETE";
+
+  event.context.apiResponse = apiResponse;
+  if (!event.context.auth) {
+    customErrorMessages.push({
+      expectedFrom: errorExpectedFroms.User,
+      reason: errorReasons.Unauthorized,
+    });
+    const { errors } = apiResponseHandler(event, customErrorMessages);
+    throw createError(errors);
+  }
+  const userId = event.context.auth.user.id;
+  console.log(userId);
+  
+  const serverId = event.context.body;
+  console.log(serverId)
+  if(!serverId)
+  {
+    customErrorMessages.push({
+      expectedFrom: errorExpectedFroms.Server,
+      reason: errorReasons.MissingParameters,
+    });
+    const { errors } = apiResponseHandler(event, customErrorMessages);
+    throw createError(errors);
+  }
+  apiResponse.params = {
+    severId : serverId,
+  }
+
+  await deleteRecord("server", serverId, customErrorMessages);
+  await prisma.server_User_Connect.deleteMany({
+    where: {
+      user_id: userId,
+    },
+  })
+
+  if (customErrorMessages.length > 0) {
+    const { errors } = apiResponseHandler(event, customErrorMessages);
+    throw createError(errors);
+  }
+  await apiResponseHandler(event,customErrorMessages,{deleted:true, totalItems:0,items:[]});
+})
