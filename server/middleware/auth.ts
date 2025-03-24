a2a1b7d8d92f7fed57265c29aadec933e2cdea8e
413ksz <developer413ksz@gmail.com>
2025-03-24 17:09:54 +0100

    docs: :memo: Added documentation for the auth middleware
    Added documentation for the auth middleware

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,3 +1,13 @@
+/**
+ * Nuxt middleware to handle authentication.
+ *
+ * This middleware will check if an `Authorization` header is present and if it is a valid token.
+ * If the token is valid, it will add an `auth` property
+ * to the `event.context` object with the user data.
+ *
+ * @param {import('nuxt').MiddlewareContext} event - The Nuxt middleware context.
+ * @returns {Promise<void>}
+ */
 export default defineEventHandler(async (event) => {
     
     const token = getHeader(event, "Authorization")?.replace("Bearer ", '')
