d9639951f48e3230ad4b9524ab7a9f7ade55827c
413ksz <developer413ksz@gmail.com>
2025-03-24 17:12:05 +0100

    docs: :memo: Added documentation for the protected middleware
    Added documentation for the protected middleware

--- middleware/protected.ts
+++ middleware/protected.ts
@@ -1,3 +1,10 @@
+/**
+ * This middleware is used to protect routes that require the user to be logged in.
+ * It checks if the user has a valid token, and if not, redirects them to the login page.
+ *
+ * @param to - The route that the user is trying to access
+ * @param from - The route that the user is coming from
+ */
 export default defineNuxtRouteMiddleware(async (to, from) => {
 
     const { getToken, tokenRefresh, tokenStatus } = useToken()
