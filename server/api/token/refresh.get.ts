8744e3e2ac4c1903793e256a2cf8951d74ae84a8
413ksz <developer413ksz@gmail.com>
2025-04-15 21:37:39 +0200

    fix: :bug: Fixed type use tokenvalidation instead of uservalidation not defined variable
    Fixed type use tokenvalidation instead of uservalidation not defined variable

--- server/api/token/refresh.get.ts
+++ server/api/token/refresh.get.ts
@@ -1,61 +1,69 @@
 import prisma from "~/lib/prisma";
-import { errorExpectedFroms, errorReasons, type CustomErrorMessage } from "~/types/customErrorMessage";
+import {
+  errorExpectedFroms,
+  errorReasons,
+  type CustomErrorMessage,
+} from "~/types/customErrorMessage";
 
 export default defineEventHandler(async (event) => {
-    const config = useRuntimeConfig()
-    const apiResponse = {} as ApiResponse;
-    const errorMessages: CustomErrorMessage[] = []
+  const config = useRuntimeConfig();
+  const apiResponse = {} as ApiResponse;
+  const errorMessages: CustomErrorMessage[] = [];
 
-    apiResponse.context = "Token/Refresh";
-    apiResponse.method = "GET";
-    event.context.apiResponse = apiResponse;
+  apiResponse.context = "Token/Refresh";
+  apiResponse.method = "GET";
+  event.context.apiResponse = apiResponse;
 
+  if (!event.context.auth) {
+    throw createError({ statusCode: 401, statusMessage: "Unauthorized" });
+  }
+  if (!event.context.auth.user.verification_code) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Bad Request",
+      message: "Expected refresh token, got access token",
+    });
+  }
 
-    if (!event.context.auth) {
-        throw createError({ statusCode: 401, statusMessage: "Unauthorized" });
-    }
-    if (!event.context.auth.user.verification_code ) {
-        throw createError({ statusCode: 400, statusMessage:"Bad Request", message: "Expected refresh token, got access token" });
-    }
+  const tokenvalidation = await prisma.user.findFirst({
+    where: {
+      id: event.context.auth.user.id,
+    },
+    select: {
+      verification_code: true,
+      id: true,
+      email: true,
+      profile_picture: true,
+      username: true,
+    },
+  });
 
-    const tokenvalidation = await prisma.user.findFirst({
-        where: {
-            id: event.context.auth.user.id
-        },
-        select: {
-            verification_code: true,
-            id: true,
-            email: true,
-            profile_picture: true,
-            username: true
-        }
-    })
+  if (
+    event.context.auth.user.verification_code !==
+    tokenvalidation?.verification_code
+  ) {
+    throw createError({ statusCode: 401, statusMessage: "Unauthorized" });
+  }
 
-    if (event.context.auth.user.verification_code !== tokenvalidation?.verification_code) {
-        throw createError({ statusCode: 401, statusMessage: "Unauthorized" });
-    }
+  const tokenData = {
+    user: {
+      id: tokenvalidation?.id,
+      email: tokenvalidation?.email,
+      picture: tokenvalidation?.profile_picture,
+      username: tokenvalidation?.username,
+    },
+  };
 
-    const tokenData = {
-        user: {
-            id: userValidation?.id,
-            email: userValidation?.email,
-            picture: userValidation?.profile_picture,
-            username: userValidation?.username,
-        },
-    }
+  const token = `Bearer ${await generateJWT(tokenData, config.JWT_EXP_TIME)}`;
 
-    const token = `Bearer ${await generateJWT(tokenData, config.JWT_EXP_TIME)}`
+  apiResponse.data = {
+    totalItems: 1,
+    items: [
+      {
+        token,
+      },
+    ],
+  };
 
-
-    apiResponse.data = {
-        totalItems: 1,
-        items: [
-            {
-                token
-            }
-        ]
-    }
-
-
-    return apiResponse
-})
+  return apiResponse;
+});
