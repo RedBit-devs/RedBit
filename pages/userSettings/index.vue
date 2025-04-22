f9eacd6bf1c40be3d48ef0077c98e264a3abf40c
Kevin <kevinszigetvari40@gmail.com>
2025-04-22 18:07:05 +0200

    docs: :memo: Created the user settings pages

---
+++ pages/userSettings/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="close ">
+            <NuxtLink to="/chatPage">
+                <Icon name="mdi:close" size="550%" class="btn primary" />
+            </NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+definePageMeta({
+    layout: "user"
+})
+
+
+
+</script>
+
+<style lang="scss" scoped></style>
